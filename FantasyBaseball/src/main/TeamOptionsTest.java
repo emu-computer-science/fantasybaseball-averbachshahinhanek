b82174ca257b242967cd8240333f43d53d1d09e1
ahanek <35896978+ahanek@users.noreply.github.com>
2022-03-23 21:47:52 -0400

    Test subs: PlayersTest.java, TeamOptionsTest.java, TeamsTest.java

---
+++ FantasyBaseball/src/main/TeamOptionsTest.java
@@ -0,0 +1,53 @@
+package main;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TeamOptionsTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void ODRAFT() {
+    }
+
+    @Test
+    public void IDRAFT() {
+    }
+
+    @Test
+    public void OVERALL() {
+    }
+
+    @Test
+    public void POVERALL() {
+    }
+
+    @Test
+    public void SAVE() {
+    }
+
+    @Test
+    public void restoreFile() {
+    }
+
+    @Test
+    public void EVALFUN() {
+    }
+
+    @Test
+    public void PEVALFUN() {
+    }
+
+    @Test
+    public void teams() {
+    }
+
+    @Test
+    public void stars() {
+    }
+}
