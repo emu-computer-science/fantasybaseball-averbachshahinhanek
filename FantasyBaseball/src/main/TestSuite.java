7fbfbab0e6ee037414bdb236f2c9aa7d570eeab4
ahanek <35896978+ahanek@users.noreply.github.com>
2022-03-24 09:17:21 -0400

    Added Test suite to test all the test at once.

---
+++ FantasyBaseball/src/main/TestSuite.java
@@ -0,0 +1,16 @@
+package main;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import static org.junit.Assert.*;
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        PlayersTest.class,
+        TeamOptionsTest.class,
+        TeamOptionsTest.class
+})
+public class TestSuite {
+
+}
