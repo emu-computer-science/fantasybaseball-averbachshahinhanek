682e9692c9bd4d99108c1f1ff8fa5e8eb6d0c6c8
kidreveloution <alyeldinshahin@gmail.com>
2022-04-10 18:55:04 -0400

    Fixing up the evalfun to accept multple level strings

--- FantasyBaseball/src/testing/p_e_valfun.java
+++ FantasyBaseball/src/testing/p_e_valfun.java
@@ -1,5 +1,7 @@
 package testing;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Stack;
 
 class Node{
@@ -50,10 +52,80 @@ public class p_e_valfun {
         System.out.print(root.data);
         inorder(root.right);
     }
-    public static void main(String[] args) {
-        String postfix = "3 + ((5+9)*2)";
 
-        Node r = expressionTree(postfix);
+    public static void evalfunStart(String starter){
+        //AVG, OBP, AB, SLG, SB.
+        String[] postfix = starter.split("\\s+");
+        ArrayList<String> characters = new ArrayList<String>();
+        ArrayList<String> operators = new ArrayList<String>();
+
+
+        System.out.println(Arrays.toString(postfix));
+        for (int i=0;i<postfix.length;i++) {
+            switch (postfix[i]) {
+                case "AVG" ->  //AVG = A
+                        //postfix[i] = String.valueOf('A');
+                        characters.add("A");
+                case "OBP" ->  //OBP = O
+                        //postfix[i] = String.valueOf('O');
+                        characters.add("O");
+
+                case "AB" ->  //AB = B
+                        //postfix[i] = String.valueOf('B');
+                        characters.add("B");
+
+                case "SLG" ->  //SLG = S
+                        //postfix[i] = String.valueOf('S');
+                        characters.add("S");
+
+                case "SB" ->  //SB = F
+                        //postfix[i] = String.valueOf('F');
+                        characters.add("F");
+
+                case "+" -> {
+                    operators.add(0,"+");//operators
+                }
+                case "-" -> {//operators
+                    operators.add(0,"-");//operators
+
+                }
+                case "/" -> {//operators
+                    operators.add(0,"/");//operators
+
+                }
+                case "*" -> {//operators
+                    operators.add(0,"*");//operators
+
+                }
+                default ->{
+                    characters.add(0,postfix[i]);
+                }
+
+            }
+
+
+        }
+        characters.addAll(operators);
+
+
+
+
+        String c = String.join("",characters);
+        System.out.println(c);
+        Node r = expressionTree(c);
         inorder(r);
+
+        //System.out.println(characters);
+        //System.out.println(operators);
+
+
+
+    }
+    public static void main(String[] args) {
+        ////AVG, OBP, AB, SLG, SB.
+        String postfix = "34 * OBP + SLG - SB";
+        evalfunStart(postfix);
+        //Node r = expressionTree(postfix);
+        //inorder(r);
     }
 }
