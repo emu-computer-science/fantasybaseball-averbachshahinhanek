275099088571e49d4078d4648fcd2fcf8159e16e
ahanek <ahanek@wccnet.edu>
2022-04-10 15:34:53 -0400

    Small changes to the help command and added equalIgnorecase to argument[0] for faster checking.

--- FantasyBaseball/src/testing/Main.java
+++ FantasyBaseball/src/testing/Main.java
@@ -28,44 +28,51 @@ public class Main {
         DataBase db = new DataBase();
         db.initializePitcher(new File("Pitchers.txt"));
         db.initializeBatters(new File("Batters.txt"));
-
+        help();
 
         while(true) {
              System.out.print("->");
              String input = reader.readLine();
              arguments = input.split(" ");
 
-
-            if(arguments[0].equals("ODRAFT") && arguments.length - 1 != 2) {
+            if(arguments[0].equalsIgnoreCase("ODRAFT") && arguments.length - 1 != 2) {
                 arguments[1] = arguments[1].substring(1);
                 arguments[2] = arguments[2].substring(0,arguments[2].length() - 1);
                 db.getPlayer((arguments[1] + " " + arguments[2]), mlb.getTeam(arguments[3]));
-            } else if(arguments[0].equals("IDRAFT")) {
+            } else if(arguments[0].equalsIgnoreCase("IDRAFT")) {
                 arguments[1] = arguments[1].substring(1);
                 arguments[2] = arguments[2].substring(0,arguments[2].length() - 1);
                 db.getPlayer((arguments[1] + " " + arguments[2]), mlb.getTeam(0)); //Uses the primary team.
 
-            } else if(arguments[0].equals("OVERALL") && arguments.length - 1 < 1) {
+            } else if(arguments[0].equalsIgnoreCase("OVERALL") && arguments.length - 1 < 1) {
                 mlb.getTeam(0).displayPos(db.getAllNonPitchers());
-            } else if(arguments[0].equals("OVERALL")) {
+            } else if(arguments[0].equalsIgnoreCase("OVERALL")) {
                 mlb.getTeam(0).displayPos(arguments[1],db.getAllNonPitchers()); //Client team
-            } else if(arguments[0].equals("POVERALL")) {
+            } else if(arguments[0].equalsIgnoreCase("POVERALL")) {
                 mlb.getTeam(0).displayPitch(db.getPitcher());
-            } else if(arguments[0].equals("TEAM") && arguments.length - 1 == 1 && arguments.length - 1 != 0){
+            } else if(arguments[0].equalsIgnoreCase("TEAM") && arguments.length - 1 == 1 && arguments.length - 1 != 0){
                 mlb.getTeam(arguments[1]).team(); //Prints out team in order of position
             } else if (arguments[0].equalsIgnoreCase("Quit")) { //Exits the Program
                 System.out.println("Thank you for using our draft system.\nGoodbye!");
                 break;
             } else if(arguments[0].equalsIgnoreCase("help")){
-                System.out.println("Please enter one of the following commands:\n\tODRAFT [playername] [league member]\n\t" +
-                        "IDRAFT [playername]\n\tOVERALL [position]\n\tPOVERALL\n\tTEAM [team name]\n\tHelp\n\tQuit");
+                help();
             } else if(arguments[0].equalsIgnoreCase("EVALFUN")){
                 System.out.println(arguments[1]);
             } else if(arguments[0].equalsIgnoreCase("PVALFUN")){
                 System.out.println(arguments[1]);
-            } else {
+            } else if(arguments[0].equalsIgnoreCase("save")){
+                System.out.println("save");
+            } else if(arguments[0].equalsIgnoreCase("restore")){
+                System.out.println("restore");
+            }else {
                 System.out.println("Invalid command\n");
             }
         }
+    }
+
+    static void help(){
+        System.out.println("Please enter one of the following commands:\n\tODRAFT [playername] [league member]\n\t" +
+                "IDRAFT [playername]\n\tOVERALL [position]\n\tPOVERALL\n\tTEAM [team name]\n\tHelp\n\tQuit");
     }
 }
