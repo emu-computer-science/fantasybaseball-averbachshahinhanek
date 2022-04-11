9548facbafb24bff35396e6457f19eb372f2e627
kidreveloution <alyeldinshahin@gmail.com>
2022-04-11 03:37:24 -0400

    finished logic of the function
    Next, I need to add a column in the database so it can later be sorted

--- FantasyBaseball/src/testing/p_e_valfun.java
+++ FantasyBaseball/src/testing/p_e_valfun.java
@@ -1,91 +1,76 @@
 package testing;
+import de.congrace.exp4j.Calculable;
+import de.congrace.exp4j.ExpressionBuilder;
+import de.congrace.exp4j.UnknownFunctionException;
+import de.congrace.exp4j.UnparsableExpressionException;
+import org.mockito.internal.matchers.Null;
 
-import java.util.ArrayList;
+import javax.script.ScriptException;
+import java.io.*;
 import java.util.Arrays;
-import java.util.Stack;
 
+
+////AVG, OBP, AB, SLG, SB.
 
 public class p_e_valfun {
 
-    public static float getValue(String columName){
-        // playerSearch = database[columName] <--- Return the value of the search
-      return 20;
-    };
+    public static double evalFun(String evalExp, String[] playerStat) throws UnparsableExpressionException, UnknownFunctionException, FileNotFoundException {
+
+        System.out.println(playerStat[0]);
+
+        Calculable calc = new ExpressionBuilder(evalExp)
+                .withVariableNames("AVG", "OBP", "AB", "SLG", "SB")
+                .build();
+        calc.setVariable("AVG", Double.parseDouble(playerStat[9]));; //<-- Here is where we would call the function to get the player AVG
+        calc.setVariable("OBP", Double.parseDouble(playerStat[10]));
+        calc.setVariable("AB", Double.parseDouble((playerStat[4])));
+        calc.setVariable("SLG", Double.parseDouble(playerStat[11]));
+        calc.setVariable("SB", Double.parseDouble(playerStat[8]));
+
+        Double calculation = calc.calculate();
+        System.out.println(calculation);
+        return(calculation);
+    }
+
+    public static double getValue(String evalFunction) throws FileNotFoundException {
 
 
-
-
-    public static void evalfunCreate(String starter){
-        //AVG, OBP, AB, SLG, SB.
-        String[] postfix = starter.split("\\s+");
-        ArrayList<String> characters = new ArrayList<String>();
-        ArrayList<String> operators = new ArrayList<String>();
-        float total = 0;
-
-        System.out.println(Arrays.toString(postfix));
-        for (int i=0;i<postfix.length;i++) {
-            switch (postfix[i]) {
-                case "AVG" -> {  //AVG = A
-                    float AVG = 56;
-                    total += AVG;
+        Integer x = 0;
+        String line = null;
+        String splitBy = " ";
+        try
+        {
+//parsing a CSV file into BufferedReader class constructor
+            BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\aladdin\\Desktop\\fantasybaseball-averbachshahinhanek\\Batters.txt"));
+            String line1=null;
+            while ((line1 = reader.readLine()) != null)  //returns a Boolean value
+            {
+                if (x == 0) {
+                    x = x + 1;
                 }
-                case "OBP" -> {
-                    float OBP = 20;
-                    total += OBP;
-                }
-
-                case "AB" -> {
-                    float AB = 10;
-                    total += AB;
-                }
-
-                case "SLG" -> {
-                    float SLG = 19;
-                    total += SLG;
-                }
-
-                case "SB" -> {
-
-                    float AVG = 56;
-                    total += AVG;
-                }
-
-                case "+" -> {
-                    operators.add(0,"+");//operators
-                }
-                case "-" -> {//operators
-                    operators.add(0,"-");//operators
+                else{
+                    String[] playerStat = line1.split(splitBy);
+                    System.out.println(playerStat[0]);
+                    evalFun(evalFunction,playerStat);
 
                 }
-                case "/" -> {//operators
-                    operators.add(0,"/");//operators
-
-                }
-                case "*" -> {//operators
-                    operators.add(0,"*");//operators
-
-                }
-                default ->{
-                    characters.add(0,postfix[i]);
-                }
-
             }
-
-
+        } catch (IOException | UnparsableExpressionException | UnknownFunctionException e) {
+            e.printStackTrace();
         }
-        characters.addAll(operators);
+        return 0;
+    }
 
 
 
 
-        System.out.println(characters);
+    public static void main(String[] args) throws ScriptException, UnparsableExpressionException, UnknownFunctionException, FileNotFoundException {
 
+        getValue("(SLG + SB) - 2");
+        String sTest = "SLG + SB - 2";
+        //evalfun(sTest);
 
-
+    }
 
 
     }
-    public static void main(String[] args) {
-
-    }
-}
