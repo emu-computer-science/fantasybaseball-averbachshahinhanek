6a53de76e8af89bafea76ce5731fc109012fb2b5
ahanek <ahanek@wccnet.edu>
2022-04-12 09:21:15 -0400

    restoring p_e_valfun - updated with java18

--- FantasyBaseball/src/testing/p_e_valfun.java
+++ FantasyBaseball/src/testing/p_e_valfun.java
@@ -4,32 +4,26 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Stack;
 
-class Node
-{
+class Node{
     char data;
-    Node left, right;
-
-    public Node(char data)
-    {
+    Node left,right;
+    public Node(char data){
         this.data = data;
         left = right = null;
     }
 }
 
-public class p_e_valfun
-{
+public class p_e_valfun {
 
-    public static void inorder(Node root)
-    {
-        if (root == null) return;
+    public static void inorder(Node root){
+        if(root==null) return;
 
         inorder(root.left);
         System.out.print(root.data);
         inorder(root.right);
     }
 
-    public static void evalfunCreate(String starter)
-    {
+    public static void evalfunCreate(String starter){
         //AVG, OBP, AB, SLG, SB.
         String[] postfix = starter.split("\\s+");
         ArrayList<String> characters = new ArrayList<String>();
@@ -37,56 +31,53 @@ public class p_e_valfun
 
 
         System.out.println(Arrays.toString(postfix));
-        for (int i = 0; i < postfix.length; i++)
-        {
-            switch (postfix[i])
-            {
-                case "AVG":  //AVG = A
-                    //postfix[i] = String.valueOf('A');
-                    characters.add("A");
-                    break;
-                case "OBP":  //OBP = O
-                    //postfix[i] = String.valueOf('O');
-                    characters.add("O");
-                    break;
+        for (int i=0;i<postfix.length;i++) {
+            switch (postfix[i]) {
+                case "AVG" ->  //AVG = A
+                        //postfix[i] = String.valueOf('A');
+                        characters.add("A");
+                case "OBP" ->  //OBP = O
+                        //postfix[i] = String.valueOf('O');
+                        characters.add("O");
 
-                case "AB":  //AB = B
-                    //postfix[i] = String.valueOf('B');
-                    characters.add("B");
-                    break;
+                case "AB" ->  //AB = B
+                        //postfix[i] = String.valueOf('B');
+                        characters.add("B");
 
-                case "SLG":  //SLG = S
-                    //postfix[i] = String.valueOf('S');
-                    characters.add("S");
-                    break;
+                case "SLG" ->  //SLG = S
+                        //postfix[i] = String.valueOf('S');
+                        characters.add("S");
 
-                case "SB":  //SB = F
-                    //postfix[i] = String.valueOf('F');
-                    characters.add("F");
-                    break;
+                case "SB" ->  //SB = F
+                        //postfix[i] = String.valueOf('F');
+                        characters.add("F");
 
-                case "+":
-                    operators.add(0, "+");//operators
-                    break;
-                case "-":
-                    operators.add(0, "-");//operators
-                    break;
+                case "+" -> {
+                    operators.add(0,"+");//operators
+                }
+                case "-" -> {//operators
+                    operators.add(0,"-");//operators
 
-                case "/":
-                    operators.add(0, "/");//operators
-                    break;
-                case "*":
-                    operators.add(0, "*");//operators
-                    break;
-                default:
-                    characters.add(0, postfix[i]);
-                    break;
+                }
+                case "/" -> {//operators
+                    operators.add(0,"/");//operators
+
+                }
+                case "*" -> {//operators
+                    operators.add(0,"*");//operators
+
+                }
+                default ->{
+                    characters.add(0,postfix[i]);
+                }
 
             }
 
 
         }
         characters.addAll(operators);
+
+
 
 
         System.out.println(operators);
@@ -96,10 +87,9 @@ public class p_e_valfun
         //System.out.println(operators);
 
 
-    }
 
-    public static void main(String[] args)
-    {
+    }
+    public static void main(String[] args) {
         ////AVG, OBP, AB, SLG, SB.
         String postfix = "34 * OBP + SLG - SB";
         //evalfunStart(postfix);
