b68483e7f272ed650332a11cdc93e0e86a1cba01
ahanek <ahanek@wccnet.edu>
2022-04-12 06:26:03 -0400

    Added test template for Database.java and fixed syntax error with p_e_valfun.java

---
+++ FantasyBaseball/src/testing/DataBaseTest.java
@@ -0,0 +1,76 @@
+package testing;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DataBaseTest
+{
+
+    @Before
+    public void setUp() throws Exception
+    {
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+    }
+
+    @Test
+    public void getPlayer()
+    {
+    }
+
+    @Test
+    public void getCatchers()
+    {
+    }
+
+    @Test
+    public void getFirstBase()
+    {
+    }
+
+    @Test
+    public void getSecondBase()
+    {
+    }
+
+    @Test
+    public void getThirdBase()
+    {
+    }
+
+    @Test
+    public void getSS()
+    {
+    }
+
+    @Test
+    public void getLeftField()
+    {
+    }
+
+    @Test
+    public void getCenterField()
+    {
+    }
+
+    @Test
+    public void getRightField()
+    {
+    }
+
+    @Test
+    public void getAllNonPitchers()
+    {
+    }
+
+    @Test
+    public void getPitcher()
+    {
+    }
+}
--- FantasyBaseball/src/testing/p_e_valfun.java
+++ FantasyBaseball/src/testing/p_e_valfun.java
@@ -4,26 +4,32 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Stack;
 
-class Node{
+class Node
+{
     char data;
-    Node left,right;
-    public Node(char data){
+    Node left, right;
+
+    public Node(char data)
+    {
         this.data = data;
         left = right = null;
     }
 }
 
-public class p_e_valfun {
+public class p_e_valfun
+{
 
-    public static void inorder(Node root){
-        if(root==null) return;
+    public static void inorder(Node root)
+    {
+        if (root == null) return;
 
         inorder(root.left);
         System.out.print(root.data);
         inorder(root.right);
     }
 
-    public static void evalfunCreate(String starter){
+    public static void evalfunCreate(String starter)
+    {
         //AVG, OBP, AB, SLG, SB.
         String[] postfix = starter.split("\\s+");
         ArrayList<String> characters = new ArrayList<String>();
@@ -31,53 +37,56 @@ public class p_e_valfun {
 
 
         System.out.println(Arrays.toString(postfix));
-        for (int i=0;i<postfix.length;i++) {
-            switch (postfix[i]) {
-                case "AVG" ->  //AVG = A
-                        //postfix[i] = String.valueOf('A');
-                        characters.add("A");
-                case "OBP" ->  //OBP = O
-                        //postfix[i] = String.valueOf('O');
-                        characters.add("O");
+        for (int i = 0; i < postfix.length; i++)
+        {
+            switch (postfix[i])
+            {
+                case "AVG":  //AVG = A
+                    //postfix[i] = String.valueOf('A');
+                    characters.add("A");
+                    break;
+                case "OBP":  //OBP = O
+                    //postfix[i] = String.valueOf('O');
+                    characters.add("O");
+                    break;
 
-                case "AB" ->  //AB = B
-                        //postfix[i] = String.valueOf('B');
-                        characters.add("B");
+                case "AB":  //AB = B
+                    //postfix[i] = String.valueOf('B');
+                    characters.add("B");
+                    break;
 
-                case "SLG" ->  //SLG = S
-                        //postfix[i] = String.valueOf('S');
-                        characters.add("S");
+                case "SLG":  //SLG = S
+                    //postfix[i] = String.valueOf('S');
+                    characters.add("S");
+                    break;
 
-                case "SB" ->  //SB = F
-                        //postfix[i] = String.valueOf('F');
-                        characters.add("F");
+                case "SB":  //SB = F
+                    //postfix[i] = String.valueOf('F');
+                    characters.add("F");
+                    break;
 
-                case "+" -> {
-                    operators.add(0,"+");//operators
-                }
-                case "-" -> {//operators
-                    operators.add(0,"-");//operators
+                case "+":
+                    operators.add(0, "+");//operators
+                    break;
+                case "-":
+                    operators.add(0, "-");//operators
+                    break;
 
-                }
-                case "/" -> {//operators
-                    operators.add(0,"/");//operators
-
-                }
-                case "*" -> {//operators
-                    operators.add(0,"*");//operators
-
-                }
-                default ->{
-                    characters.add(0,postfix[i]);
-                }
+                case "/":
+                    operators.add(0, "/");//operators
+                    break;
+                case "*":
+                    operators.add(0, "*");//operators
+                    break;
+                default:
+                    characters.add(0, postfix[i]);
+                    break;
 
             }
 
 
         }
         characters.addAll(operators);
-
-
 
 
         System.out.println(operators);
@@ -87,9 +96,10 @@ public class p_e_valfun {
         //System.out.println(operators);
 
 
-
     }
-    public static void main(String[] args) {
+
+    public static void main(String[] args)
+    {
         ////AVG, OBP, AB, SLG, SB.
         String postfix = "34 * OBP + SLG - SB";
         //evalfunStart(postfix);
