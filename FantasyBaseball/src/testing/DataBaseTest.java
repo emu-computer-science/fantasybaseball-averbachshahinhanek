cb1ef9a25042f6b781d9cd3c5e674125a5f55c39
ahanek <ahanek@wccnet.edu>
2022-04-21 00:05:55 -0400

    backup point

--- FantasyBaseball/src/testing/DataBaseTest.java
+++ FantasyBaseball/src/testing/DataBaseTest.java
@@ -5,25 +5,57 @@ import org.junit.Before;
 import org.junit.Test;
 import testing.DataBase;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
 public class DataBaseTest
 {
 DataBase dataBase;
+    ArrayList<Pitcher> pitcher;
     @Before
     public void setUp() throws Exception
     {
         dataBase = new DataBase("Pitchers.txt", "Batters.txt");
-//        dataBase.initializePitcher(new File(getClass().getClassLoader().getResource("Pitchers.txt").getFile()));
-//        dataBase.initializeBatters(new File(getClass().getClassLoader().getResource("Batters.txt").getFile()));
+        pitcher = new ArrayList<>();
     }
 
     @After
     public void tearDown() throws Exception
     {
     }
+
+@Test
+public void getInitializePitcher()
+{
+    dataBase.setPitchersDatabase("Pitchers.txt");
+    try (BufferedReader reader = new BufferedReader(new FileReader(dataBase.getPitchersDatabase())))
+    {
+        String data; //Holds contents of a line of the database
+        String[] line = new String[9];
+        int tempC = 0;
+        //To store pitcher data
+        reader.readLine(); //Reads first line of the database
+        while ((data = reader.readLine()) != null)
+        {
+            line = data.split(" ");
+
+            line[0] = line[0].substring(0, 1);
+            line[1] = line[1] + ",";
+            //Format example: [Last name, first initial, team, position, IP]
+            pitcher.add(new Pitcher((line[1] + " " + line[0]), line[2], "P", Double.parseDouble(line[3]),
+                    Double.parseDouble(line[4]), Double.parseDouble(line[5]), Double.parseDouble(line[6]),
+                    Double.parseDouble(line[7]), Double.parseDouble(line[8])));
+        }
+    } catch (IOException e)
+    {
+        System.out.println("Error reading the file");
+    }
+}
 
     @Test
     public void getPlayer()
